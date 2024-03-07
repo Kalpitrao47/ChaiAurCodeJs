9f1b1053e1d1c5e3b4816174b310ae7830b850f5
KalpitRao47 <kalpitraorane47@gmail.com>
2024-03-07 13:16:21 +0530

    data_types

--- Basics/data_types.js
+++ Basics/data_types.js
@@ -1,3 +1,21 @@
 //Type Conversion
-let score = 546
-console.log(typeof score);
+// let score = 546
+// console.log(typeof score);
+
+// console.log(1+2);
+// console.log("1"+2);
+// console.log("1"+"2");
+// console.log(1+"2");
+// console.log(1+2+"2");
+
+
+let x = 3;
+let y = x++;
+console.log("y ka value",y);
+console.log("x ka value",x);
+
+let a = 5;
+let b = ++a;
+console.log("b ka value",b);
+console.log("a ka value",a);
+
