42467ec10d494e743e5d92bccdafd851570a0a06
KalpitRao47 <kalpitraorane47@gmail.com>
2024-03-07 15:40:11 +0530

    Strings

---
+++ Basics/strings.js
@@ -0,0 +1,22 @@
+let name = "Rohan";
+let age = 27
+
+console.log(`My name is ${name} and my age is ${age}`);
+
+
+const gameName = new String("Kalpit Raorane");
+console.log(gameName)
+console.log(gameName.charAt([2]));
+console.log(gameName.indexOf('i'));
+console.log(gameName.substring(0,4));
+console.log(gameName.substring(3,5));
+console.log(gameName.concat("hie"));
+console.log(gameName.includes("z"));
+console.log(gameName.replace("t","z"));
+console.log(gameName.slice(1,3));
+console.log(gameName.trim()); //Remove whitespaces form both sides of a string
+
+
+
+
+
