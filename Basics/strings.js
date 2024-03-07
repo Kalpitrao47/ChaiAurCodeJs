a93f3f5df68c40ae9141e417f8aabaaade828576
KalpitRao47 <kalpitraorane47@gmail.com>
2024-03-07 16:52:32 +0530

    Strings

--- Basics/strings.js
+++ Basics/strings.js
@@ -8,13 +8,15 @@ const gameName = new String("Kalpit Raorane");
 console.log(gameName)
 console.log(gameName.charAt([2]));
 console.log(gameName.indexOf('i'));
-console.log(gameName.substring(0,4));
-console.log(gameName.substring(3,5));
+console.log(gameName.substring(0,4));//Kalp
+console.log(gameName.substring(3,5));//pi
 console.log(gameName.concat("hie"));
 console.log(gameName.includes("z"));
-console.log(gameName.replace("t","z"));
-console.log(gameName.slice(1,3));
+console.log(gameName.replace("t","z"));//will replace t with z
+console.log(gameName.slice(1,3));//al
+console.log(gameName.slice(-1.-3));//rane
 console.log(gameName.trim()); //Remove whitespaces form both sides of a string
+console.log(gameName.split(","));
 
 
 
