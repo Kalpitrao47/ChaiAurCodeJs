5f3c60344383ae9863a882e1cecc13b109eeec91
KalpitRao47 <kalpitraorane47@gmail.com>
2024-03-09 12:11:52 +0530

    Arrays02

--- Basics02/Arrays02.js
+++ Basics02/Arrays02.js
@@ -13,3 +13,23 @@ console.log(allNewHeroes);
 
 const FlatHeroes = [1,2,3,4,5,[6,7],8,9,[10,11,12,[,13,14,15]]];
 console.log(FlatHeroes.flat(Infinity));
+
+//Convert to Array  
+console.log(Array.isArray("KalpitRaorane"));  
+
+//Array.from
+console.log(Array.from("KalpitRaorane"));
+console.log(Array.from({
+    name:"KalpitRaorane",
+    email:"kalpitraane@gmail.com",
+}));   
+
+//Array.of
+//The Array.of() method creates a new array from any number of arguments.
+// The Array.of() method can take any type of arguments.
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
